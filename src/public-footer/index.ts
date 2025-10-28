f37e4159dffc53f3a4192ff9d158d144b78bfaec
Miles Watkins <miles.w.watkins@gmail.com>
2025-10-27 23:40:49 -0700

    Add console messages with information on how to discover and disable plugins

--- src/public-footer/index.ts
+++ src/public-footer/index.ts
@@ -1,3 +1,7 @@
 import "./grouped-items-text.ts";
 import "./uncheck-auto-renew.ts";
 import "./collect-item-metrics.ts";
+
+console.info(
+  `Several unofficial plugins are being injected into this MyTurn page to add or improve functionality. However, if MyTurn's markup changes then these plugins may cause breaking errors. If they do seem to be causing errors, you can fix or disable plugins individually via the GitHub repository https://github.com/Sustainable-Capitol-Hill/myturn-hacks or *if absolutely necessary* you can disable all of them via the "Footer Includes" options in MyTurn's Site Customization settings page.`,
+);
