119d3594505eb9294419b42ab72237cc3e9a6f69
Sam Wolfson <terabyte128@gmail.com>
2025-11-01 22:07:15 -0700

    Add development server
    Improves the local dev experience by running a local dev server that
    proxies requests to MyTurn, replacing hack script references with ones
    that are bundled locally.

---
+++ src/proxy.ts
@@ -0,0 +1,104 @@
+import express from "express";
+import * as path from "path";
+import * as esbuild from "esbuild";
+import { parse } from "node-html-parser";
+import type { Request, Response } from "express";
+
+/*
+ * This script runs a proxy server that:
+ * - serves bundled scripts at /devscripts/{script}.js
+ * - proxies everything else to MyTurn
+ * - injects middleware which replaces <script> tags referencing known scripts
+ *   with the ones served locally.
+ */
+
+const HOST = "localhost";
+const PORT = 3000;
+const MYTURN = "https://capitolhill.myturn.com";
+const SCRIPTS = ["public-footer", "admin-footer"];
+
+import {
+  createProxyMiddleware,
+  responseInterceptor,
+} from "http-proxy-middleware";
+
+const app = express();
+
+const bundle = async (folder: (typeof SCRIPTS)[number]): Promise<string> => {
+  const entrypoint = path.join(import.meta.dirname, folder);
+  const output = await esbuild.build({
+    entryPoints: [entrypoint],
+    bundle: true,
+    minify: true,
+    sourcemap: "inline",
+    target: "es2015",
+    write: false,
+  });
+
+  return output.outputFiles[0]?.text ?? "";
+};
+
+const proxyMiddleware = createProxyMiddleware<Request, Response>({
+  target: MYTURN,
+  changeOrigin: true,
+  selfHandleResponse: true,
+  on: {
+    proxyRes: responseInterceptor(
+      async (responseBuffer, proxyRes, req, res) => {
+        const contentType = proxyRes.headers["content-type"];
+
+        // redirect everything back to proxy
+        if (proxyRes.headers.location) {
+          res.setHeader(
+            "location",
+            proxyRes.headers.location.replaceAll(
+              MYTURN,
+              `http://${HOST}:${PORT}`,
+            ),
+          );
+        }
+
+        if (!(contentType && contentType.startsWith("text/html"))) {
+          // don't try to munge any non-html files
+          return responseBuffer;
+        }
+
+        const response = responseBuffer.toString("utf8");
+        const soup = parse(response);
+
+        // replace src of all hack scripts with our local ones
+        SCRIPTS.forEach((script) => {
+          const el = soup.querySelector(`script[src$="${script}.js"]`);
+          el?.setAttribute("id", `__devscript=${script}`);
+          el?.setAttribute(
+            "src",
+            `http://${HOST}:${PORT}/devscripts/${script}.js`,
+          );
+        });
+
+        return soup.toString();
+      },
+    ),
+  },
+});
+
+/**
+ * Serve bundled scripts at /devscripts
+ */
+app.get("/devscripts/:scriptName.js", async (req, res) => {
+  if (!SCRIPTS.includes(req.params.scriptName)) {
+    res.status(404).send();
+    return;
+  }
+
+  res.header("content-type", "text/javascript");
+  res.send(await bundle(req.params.scriptName));
+});
+
+/**
+ * Proxy everything else to MyTurn
+ */
+app.use("/", proxyMiddleware);
+
+app.listen(PORT);
+console.log(`Proxy server listening on port ${PORT}`);
